21d9e9b2e859b2b5bb7ac2525a576cb303c4d305
lucas <lucas@LWZHNBTWO>
2020-01-06 18:27:56 +0100

    TEST

--- src/ch/store/lager/TestPackage.java
+++ src/ch/store/lager/TestPackage.java
@@ -3,8 +3,15 @@ import ch.store.laden.Packages;
 import ch.store.laden.Regale;
 
 
-'test'
+'test Lucas
+
+'TEST
+
+
 public class TestPackage {
+	String tTestString = "" ;
+	
+	
 public static void main(String[] args) {
 	Packages p = new Packages();
 	Regale r = new Regale();

ff8166912e0ec14f1dd0d3a53b4e12e5ea236da9
walde <walde@10.192.100.96>
2020-01-23 19:10:01 +0100

    a

--- src/ch/store/laden/LadenMain.java
+++ src/ch/store/laden/LadenMain.java
@@ -1,12 +1,12 @@
-package ch.store.lager;
+package ch.store.laden;
 
 import ch.store.laden.Regale;
 import ch.store.laden.Terminals;
 
-public class TestPackage {
-
+public class LadenMain {
 	public static void main(String[] args) {
 		Terminals t = new Terminals();
 		Regale r = new Regale();
 	}
+
 }

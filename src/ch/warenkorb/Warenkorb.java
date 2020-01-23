1c7d8329e3aff410104147c24ba26e9b74b7e75b
walde <walde@10.192.100.96>
2020-01-23 19:44:45 +0100

    a

--- src/ch/warenkorb/Warenkorb.java
+++ src/ch/warenkorb/Warenkorb.java
@@ -1,5 +1,14 @@
 package ch.warenkorb;
 
+import java.util.Date;
+
 public class Warenkorb {
+	
+	//Attribute
+	private int warenkorbID;
+	Date warenkorbDatum = new Date(2020, 12, 31);
+	//Konstruktoren
+	
+	//Methoden
 
 }
